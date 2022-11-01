1d359607e9b08c42147857edd5b37fa05f508e88
Sahith Bhamidipati <sahithb00@gmail.com>
2022-11-01 16:02:56 -0400

    Added large model running file and fixed layer parameters

--- largeModel.py
+++ largeModel.py
@@ -2,26 +2,26 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from collections import OrderedDict
-from tensorflow import keras
+# from tensorflow import keras
 
-model = keras.models.Sequential([
-    keras.Input((227, 227, 3)),
-    keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu'),
-    keras.layers.BatchNormalization(),
-    keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-    keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
-    keras.layers.BatchNormalization(),
-    keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-    keras.layers.BatchNormalization(),
-    keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-    keras.layers.BatchNormalization(),
-    keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-    keras.layers.Flatten(),
-    keras.layers.Dense(254, activation='relu'),
-    keras.layers.Dropout(0.5),
-    keras.layers.Dense(10, activation='softmax')
-])
-model.summary()
+# model = keras.models.Sequential([
+#     keras.Input((227, 227, 3)),
+#     keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu'),
+#     keras.layers.BatchNormalization(),
+#     keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+#     keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
+#     keras.layers.BatchNormalization(),
+#     keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+#     keras.layers.BatchNormalization(),
+#     keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+#     keras.layers.BatchNormalization(),
+#     keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+#     keras.layers.Flatten(),
+#     keras.layers.Dense(254, activation='relu'),
+#     keras.layers.Dropout(0.5),
+#     keras.layers.Dense(10, activation='softmax')
+# ])
+# model.summary()
 
 '''
 # Rewrite this class with AlexNet equivalent
@@ -82,32 +82,32 @@ class ANet(nn.Module):
 
         # Conv 2 27x27x256
         self.conv2 = nn.Sequential(
-                        nn.Conv2d(3, 256, kernel_size=5, stride=1, padding=0),
-                        nn.BatchNorm2d(96),
+                        nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=0),
+                        nn.BatchNorm2d(256),
                         nn.ReLU()
                         )
 
         # Conv 3 27x27x384
         self.conv3 = nn.Sequential(
-                        nn.Conv2d(3, 384, kernel_size=3, stride=1, padding=0),
-                        nn.BatchNorm2d(96),
+                        nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=0),
+                        nn.BatchNorm2d(384),
                         nn.ReLU()
                         )
 
         # Conv 4 27x27x384
-        self.conv4 = nn.Sequential(
-                        nn.Conv2d(3, 384, kernel_size=3, stride=1, padding=0),
-                        nn.BatchNorm2d(96),
-                        nn.ReLU()
-                        )
+        # self.conv4 = nn.Sequential(
+        #                 nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=0),
+        #                 nn.BatchNorm2d(384),
+        #                 nn.ReLU()
+        #                 )
 
-        # Pool 2 13x13x384
+        # Pool 2 10x10x384
         self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2)
 
         # Dense layer 1 size 254
         self.lin1 = nn.Sequential(
                         nn.Dropout(0.5),
-                        nn.Linear(13*13*384, 254),
+                        nn.Linear(10*10*384, 254),
                         nn.ReLU()
                         )
 
@@ -122,8 +122,10 @@ class ANet(nn.Module):
         x = self.pool1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        x = self.conv4(x)
+        #x = self.conv4(x)
         x = self.pool2(x)
+
+        #print(x.shape)
 
         # flatten convolutional layer into vector
         x = x.view(x.size(0), -1)
